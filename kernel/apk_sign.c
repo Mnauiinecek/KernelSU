337da422171437129a07e8c1e4e33add4281741f
Mnauiinecek <rick62275@gmail.com>
2026-03-31 23:52:20 +0200

    bruh

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -361,11 +361,11 @@ bool is_manager_apk(char *path)
     }
 #endif
     return (check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH)
-        || check_v2_signature(path, 0x377, d3469712b6214462764a1d8d3e5cbe1d6819a0b629791b9f4101867821f1df64) // ReSukiSU
+        || check_v2_signature(path, 0x377, "d3469712b6214462764a1d8d3e5cbe1d6819a0b629791b9f4101867821f1df64") // ReSukiSU
         );
 #ifdef EXPECTED_SIZE2
     return (check_v2_signature(path, EXPECTED_SIZE2, EXPECTED_HASH2)
-        || check_v2_signature(path, 0x377, d3469712b6214462764a1d8d3e5cbe1d6819a0b629791b9f4101867821f1df64) // ReSukiSU
+        || check_v2_signature(path, 0x377, "d3469712b6214462764a1d8d3e5cbe1d6819a0b629791b9f4101867821f1df64") // ReSukiSU
         );
 #else
     return false;
